6be6b166652e22c665bcd7f4d25e2f3daa689bd8
OPTIMUS6577 <suxrob.com.3202412@gmail.com>
2026-05-02 10:49:53 +0500

    Bust cache: Update SW to v1.5 and switch to Network-first strategy

--- sw.js
+++ sw.js
@@ -1,21 +1,36 @@
-const CACHE_NAME = 'workhub-v1';
+const CACHE_NAME = 'workhub-v1.5';
 const ASSETS = [
   './',
   './index.html',
-  './style.css',
-  './app.js',
+  './style.css?v=1.5',
+  './app.js?v=1.5',
   './manifest.json',
   './icon-512.png'
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => {
+          return cacheName !== CACHE_NAME;
+        }).map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
